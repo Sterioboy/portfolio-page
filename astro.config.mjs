8b6bb71815d766b3a7c9306ee0ea46e767d46dfb
Kirill Bondarenko <billionmann@gmail.com>
2023-05-13 16:30:03 +0300

    hotfix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), image()],
+  integrations: [react(), tailwind(), image(), astroImageTools],
 
   experimental: {
     assets: true,
